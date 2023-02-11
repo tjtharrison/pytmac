8057dae05e337e6fbe5300d2547ca9d0a389f1fa
tjtharrison <gh@tjth.co>
2023-02-11 14:46:20 +0000

    Added some more defaults

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import json
 from datetime import date
 from copy import deepcopy
+import logging
 
 date_today = str(date.today())
+
+# Configure logging
+logging.basicConfig(level="INFO")
+logging.info("Creating handler")
+logging.Formatter(
+    '{"time": "%(asctime)s", "level": "%(levelname)s", "message": "%(message)s"}'
+)
 
 # Load resources
 with open("docs/resources.json", "r") as resources_file:
@@ -21,10 +29,14 @@ resources = resources_json["resources"]
 output_file_dir = "reports"
 output_file_name = "report-" + str(date_today)
 
-with open(output_file_dir + "/" + output_file_name + ".md", "w", encoding="UTF-8") as output_file:
+with open(
+    output_file_dir + "/" + output_file_name + ".md", "w", encoding="UTF-8"
+) as output_file:
 
     # Build out json report
-    with open(output_file_dir + "/" + output_file_name + ".json", "w", encoding="UTF-8") as output_json:
+    with open(
+        output_file_dir + "/" + output_file_name + ".json", "w", encoding="UTF-8"
+    ) as output_json:
 
         # Start empty json for output report
         output_json_report = {}
@@ -42,19 +54,27 @@ with open(output_file_dir + "/" + output_file_name + ".md", "w", encoding="UTF-8
 
         # Write networks wrapper
         output_json_report["networks"] = {}
+        output_json_report["databases"] = {}
+        output_json_report["users"] = {}
+        output_json_report["systems"] = {}
+        output_json_report["containers"] = {}
         # Process network resources as top wrapper
         for network in resources["networks"]:
 
             # Build out network config in output
-            output_json_report["networks"][network["name"]] = deepcopy(defaults_json["networks"])
+            output_json_report["networks"][network["name"]] = deepcopy(
+                defaults_json["networks"]
+            )
 
-            # Look for override config
+            # Look for override config for network
             try:
                 override_network_config = network["config"]
-                print("Overrides set for " + network["name"])
+                logging.info("Overrides set for " + network["name"])
                 for config_setting in network["config"]:
-                    print("Setting " + config_setting + " on " + network["name"])
-                    output_json_report["networks"][network["name"]][config_setting] = network["config"][config_setting]
+                    logging.info("Setting " + config_setting + " on " + network["name"])
+                    output_json_report["networks"][network["name"]][
+                        config_setting
+                    ] = network["config"][config_setting]
             except KeyError:
                 # No overrides set, nothing to do
                 pass
@@ -73,6 +93,26 @@ with open(output_file_dir + "/" + output_file_name + ".md", "w", encoding="UTF-8
             # Look for users in network
             for user in resources["users"]:
                 if user["network"] == network["name"]:
+
+                    output_json_report["users"][user["name"]] = deepcopy(
+                        defaults_json["users"]
+                    )
+                    # Look for override config
+                    try:
+                        override_user_config = user["config"]
+                        logging.info("Overrides set for " + user["name"])
+                        for config_setting in user["config"]:
+                            logging.info(
+                                "Setting " + config_setting + " on " + user["name"]
+                            )
+                            output_json_report["databases"][user["name"]][
+                                config_setting
+                            ] = user["config"][config_setting]
+                    except KeyError:
+                        # No overrides set, nothing to do
+                        logging.info("No overrides for " + user["name"])
+                        pass
+
                     output_file.write(
                         "\t\t"
                         + "Person("
@@ -88,6 +128,26 @@ with open(output_file_dir + "/" + output_file_name + ".md", "w", encoding="UTF-8
             # Look for databases in network
             for database in resources["databases"]:
                 if database["network"] == network["name"]:
+
+                    output_json_report["databases"][database["name"]] = deepcopy(
+                        defaults_json["databases"]
+                    )
+                    # Look for override config for database
+                    try:
+                        override_database_config = database["config"]
+                        logging.info("Overrides set for " + database["name"])
+                        for config_setting in database["config"]:
+                            logging.info(
+                                "Setting " + config_setting + " on " + database["name"]
+                            )
+                            output_json_report["databases"][database["name"]][
+                                config_setting
+                            ] = database["config"][config_setting]
+                    except KeyError:
+                        # No overrides set, nothing to do
+                        logging.info("No overrides for " + database["name"])
+                        pass
+
                     output_file.write(
                         "\t\t"
                         + "SystemDb("
@@ -106,13 +166,29 @@ with open(output_file_dir + "/" + output_file_name + ".md", "w", encoding="UTF-8
                 if system["network"] == network["name"]:
                     output_file.write(
                         "\t\t"
-                        + "Container("
+                        + "System("
                         + system["name"]
                         + ","
                         + '"'
                         + system["name"]
                         + ' ", "'
                         + system["description"]
+                        + '")'
+                        + "\n"
+                    )
+
+            # Look for containers in network
+            for container in resources["containers"]:
+                if container["network"] == network["name"]:
+                    output_file.write(
+                        "\t\t"
+                        + "Container("
+                        + container["name"]
+                        + ","
+                        + '"'
+                        + container["name"]
+                        + ' ", "'
+                        + container["description"]
                         + '")'
                         + "\n"
                     )
@@ -132,11 +208,11 @@ with open(output_file_dir + "/" + output_file_name + ".md", "w", encoding="UTF-8
                 + "\n"
             )
             output_file.write("\n")
-        output_file.write("UpdateLayoutConfig($c4ShapeInRow=\"3\", $c4BoundaryInRow=\"1\")" + "\n")
+        output_file.write(
+            'UpdateLayoutConfig($c4ShapeInRow="3", $c4BoundaryInRow="3")' + "\n"
+        )
         output_file.write("```")
 
         # Print final json
         final_report = json.dumps(output_json_report)
         output_json.write(final_report)
-        # print(final_report)
-
